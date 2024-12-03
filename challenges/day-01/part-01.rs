d54a5b685c9d8277895efe912d98e7f1189ac173
David Glymph <Woozl@users.noreply.github.com>
2024-12-03 11:50:24 -0500

    initial

---
+++ challenges/day-01/part-01.rs
@@ -0,0 +1,10 @@
+use std::{fs, path::Path};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let input = fs::read_to_string(Path::new("./challenges/day-01/input.txt"))?;
+
+    println!("Hello from day 01 part 1");
+    println!("Input file: {}", &input);
+
+    Ok(())
+}
